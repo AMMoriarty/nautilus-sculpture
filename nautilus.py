0bb09ecea593e548b3b96846ecc94d2bd23696df
Cassandra Phillips-Sears <cphillips.sears@gmail.com>
2016-04-05 18:29:51 -0400

    Update nautilus.py
    Defined LED_BRIGHTNESS

--- nautilus.py
+++ nautilus.py
@@ -14,6 +14,7 @@ LED_PIN = 18 #GPIO pin connected to pixels (must support PWM!)
 LED_FREQ_HZ = 800000 #LED signal frequency in hertz (usually 800khz)
 LED_DMA = 5 #DMA channel to use for generating  signal (try 5)
 LED_INVERT = False #True to invert the signal (when using NPN transistor level shift)
+LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
 
 #set BCM mode on GPIO
 GPIO.setmode (GPIO.BCM)
